ae9b8bda25cdb0259f395144bf6e70caaf17c3e0
Justin Harringa <justin@harringa.com>
2021-05-01 18:22:10 -0700

    Add puppeteer for automagic karma chrome
    https://github.com/karma-runner/karma-chrome-launcher

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,8 @@
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 module.exports = function(config) {
     config.set({
-        browsers: ['Chrome'],
+        browsers: ['ChromeHeadless'],
         frameworks: ['jasmine'],
         files: [
             'js/app.js',
